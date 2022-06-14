98df695d0a1b472240e2ba5c968e323378380b06
Thiago Souza <thiagorasouza@gmail.com>
2022-06-14 20:56:50 -0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import AddArticlePage from "./pages/AddArticle";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <AddArticlePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<AddArticlePage />} />
+    </Routes>
+  );
 }
 
 export default App;
