b837fb99dca0aafba0bd7d6f0a77e8b18c8bc1dd
Thiago Souza <thiagorasouza@gmail.com>
2022-06-14 21:37:03 -0300

    allow users to delete saved articles

--- src/pages/SavedArticles.js
+++ src/pages/SavedArticles.js
@@ -1,37 +1,53 @@
 import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
-import { getFromLocal } from "../helpers/local-storage";
+import { getFromLocal, saveToLocal } from "../helpers/local-storage";
 
 import styles from "./SavedArticles.module.css";
 
 function SavedArticlesPage() {
   const [articles, setArticles] = useState(null);
+
   useEffect(() => {
     const savedArticles = getFromLocal("articles");
     // console.log(savedArticles);
     setArticles(savedArticles);
   }, []);
 
-  const defaultArticle = {
-    name: "MDN FormData API",
-    url: "https://developer.mozilla.org/en-US/docs/Web/API/FormData",
-    date: "21/06/2022",
-    email: "thiagorasouza@gmail.com",
-  };
+  function handleDelete(articleId) {
+    const filteredArticles = articles.filter(
+      (article) => article.id !== articleId
+    );
+    saveToLocal("articles", filteredArticles);
+    setArticles(filteredArticles);
+  }
+
+  console.log(articles);
 
   return (
     <Layout>
       {articles
-        ? articles.map((article, index) => (
-            <div key={index} className={styles.article}>
-              <div className={styles.nameAndDate}>
-                {article.name} - review at {article.date}
+        ? Array.isArray(articles) && articles.length === 0
+          ? "No articles saved yet"
+          : articles.map((article) => (
+              <div key={article.id} className={styles.article}>
+                <div className={styles.data}>
+                  <div className={styles.nameAndDate}>
+                    {article.name} - review at {article.date}
+                  </div>
+                  <div className={styles.url}>
+                    <a href={article.url}>{article.url}</a>
+                  </div>
+                </div>
+                <div className={styles.controls}>
+                  <button
+                    type="button"
+                    onClick={() => handleDelete(article.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <div className={styles.url}>
-                <a href={article.url}>{article.url}</a>
-              </div>
-            </div>
-          ))
+            ))
         : "Loading..."}
     </Layout>
   );
