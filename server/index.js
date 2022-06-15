742d5e47264c2d85de038f4d299b66dd680a49c1
Thiago Souza <thiagorasouza@gmail.com>
2022-06-15 00:27:59 -0300

    create rest api to save and retrieve articles

--- server/index.js
+++ server/index.js
@@ -8,8 +8,36 @@ const Article = require("./articles.js");
 
 mongo.connect();
 
-app.get("/api", (req, res) => {
-  res.send("Api server");
+app.use(express.json());
+
+app.post("/api/articles", async (req, res) => {
+  const articleData = req.body;
+
+  const requiredFields = ["name", "url", "date", "email"];
+  for (const requiredField of requiredFields) {
+    if (!articleData[requiredField]) {
+      return res.status(400).json(`Missing param: ${requiredField}`);
+    }
+  }
+
+  const article = new Article(articleData);
+  try {
+    const saved = await article.save();
+    return res.status(200).json(saved);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Server error");
+  }
+});
+
+app.get("/api/articles", async (req, res) => {
+  try {
+    const articles = await Article.find({}).exec();
+    return res.status(200).json(articles);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Server error");
+  }
 });
 
 app.listen("4000", () => {
